8b84ae8f44b372a176433c0ef66dd5b78116c574
roymw09 <roymw09@gmail.com>
2022-03-18 06:00:20 -0700

    Removed Eureka dependency

--- src/main/java/org/ac/cst8277/williams/roy/UserManagementServiceApplication.java
+++ src/main/java/org/ac/cst8277/williams/roy/UserManagementServiceApplication.java
@@ -2,10 +2,8 @@ package org.ac.cst8277.williams.roy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 public class UserManagementServiceApplication {
 
     public static void main(String[] args) {
