853f55bff2281685cb8c97a7c5036870bc0f3e31
roymw09 <60860251+roymw09@users.noreply.github.com>
2022-03-19 21:06:00 -0230

    Added Eureka

--- src/main/java/org/ac/cst8277/williams/roy/UserManagementServiceApplication.java
+++ src/main/java/org/ac/cst8277/williams/roy/UserManagementServiceApplication.java
@@ -3,6 +3,8 @@ package org.ac.cst8277.williams.roy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -10,6 +12,11 @@ public class UserManagementServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(UserManagementServiceApplication.class, args);
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 
 }
