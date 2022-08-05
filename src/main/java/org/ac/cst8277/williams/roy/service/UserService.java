538fec9c65b047b0af3b3b85352f73d6e2713e73
roywilliams <roymw09@gmail.com>
2022-08-05 12:57:53 -0230

    Removed Autowired annotation

--- src/main/java/org/ac/cst8277/williams/roy/service/UserService.java
+++ src/main/java/org/ac/cst8277/williams/roy/service/UserService.java
@@ -14,8 +14,11 @@ import java.util.List;
 @Transactional
 public class UserService {
 
-    @Autowired
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     public Mono<User> createUser(User user) {
         return userRepository.save(user);
