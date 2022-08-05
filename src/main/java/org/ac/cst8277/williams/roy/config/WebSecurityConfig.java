fb6424c99a08f3d9ca9fefdf80bfb6da3e97e866
roywilliams <roymw09@gmail.com>
2022-08-04 22:17:32 -0230

    Added web security config

---
+++ src/main/java/org/ac/cst8277/williams/roy/config/WebSecurityConfig.java
@@ -0,0 +1,27 @@
+package org.ac.cst8277.williams.roy.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+
+@EnableWebSecurity
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    // permitted all requests for the interview so postman will work
+    // authenticate all requests
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .anyRequest()
+                .permitAll()
+                .and()
+                .csrf()
+                .disable();
+                /*.authenticated()
+                .and()
+                .oauth2Login();*/
+    }
+}
