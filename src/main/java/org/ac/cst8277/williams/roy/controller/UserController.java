490908757f249017ca88915aaa19091a6e947808
roywilliams <roymw09@gmail.com>
2022-08-05 10:39:24 -0230

    Removed unused methods

--- src/main/java/org/ac/cst8277/williams/roy/controller/UserController.java
+++ src/main/java/org/ac/cst8277/williams/roy/controller/UserController.java
@@ -1,18 +1,13 @@
 package org.ac.cst8277.williams.roy.controller;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import org.ac.cst8277.williams.roy.model.User;
 import org.ac.cst8277.williams.roy.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.*;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
-import javax.servlet.http.HttpServletRequest;
 import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/users/user")
@@ -20,23 +15,6 @@ public class UserController {
 
     @Autowired
     private UserService userService;
-
-    @GetMapping("/user")
-    public Map<String, Object> user(@AuthenticationPrincipal OAuth2User principal) throws JsonProcessingException {
-        String name = principal.getAttribute("login");
-        User user = userService.checkIfUserExists(name).block();
-        if (user == null) {
-            userService.createUser(new User(name)).subscribe();
-        }
-        return Map.of("name", name);
-    }
-
-    @GetMapping("/error")
-    public String error(HttpServletRequest request) {
-        String message = (String) request.getSession().getAttribute("error.message");
-        request.getSession().removeAttribute("error.message");
-        return message;
-    }
 
     @PostMapping("/create")
     @ResponseStatus(HttpStatus.CREATED)
@@ -70,19 +48,18 @@ public class UserController {
                 .defaultIfEmpty(ResponseEntity.notFound().build());
     }
 
-    @PostMapping("/search/id")
+    @GetMapping("/search/id")
     public Flux<User> fetchUsersById(@RequestBody List<Integer> ids) {
         return userService.fetchUsers(ids);
     }
 
     @GetMapping("/getUsername/{userId}")
-    public Mono<String> getUsernameById(@PathVariable String userId) {
-        Integer user_id = Integer.parseInt(userId);
-        return userService.getUsernameById(user_id);
+    public Mono<String> getUsernameById(@PathVariable Integer userId) {
+        return userService.getUsernameById(userId);
     }
 
     @GetMapping("/checkUser/{username}")
     public Mono<User> checkIfUserExists(@PathVariable String username) {
-        return userService.checkIfUserExists(username);
+        return userService.checkIfUserExists(username).switchIfEmpty(userService.createUser(new User(username)));
     }
 }
