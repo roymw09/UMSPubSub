b85484af053e54a0d35cf1678dbf920cb4811cbf
roywilliams <roymw09@gmail.com>
2022-08-04 22:21:40 -0230

    Changed id, user_id, & role_id from String to Integer

--- src/main/java/org/ac/cst8277/williams/roy/model/UserRole.java
+++ src/main/java/org/ac/cst8277/williams/roy/model/UserRole.java
@@ -5,13 +5,17 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.relational.core.mapping.Table;
 
+import javax.persistence.Id;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Table("user_roles")
 public class UserRole {
+    @Id
+    private Integer id;
     private Integer user_id;
-    private String role_id;
+    private Integer role_id;
     private String role;
     private String description;
     private String refreshToken;
