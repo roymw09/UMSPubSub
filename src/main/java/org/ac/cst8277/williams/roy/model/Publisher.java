8d74545839c63c9fa8e1710b3e8afc9537eac1ff
roywilliams <roymw09@gmail.com>
2022-08-04 22:19:23 -0230

    Changed id from String to Integer & added the Id annotation

--- src/main/java/org/ac/cst8277/williams/roy/model/Publisher.java
+++ src/main/java/org/ac/cst8277/williams/roy/model/Publisher.java
@@ -1,9 +1,11 @@
 package org.ac.cst8277.williams.roy.model;
 
 import lombok.Data;
+import org.springframework.data.annotation.Id;
 
 @Data
 public class Publisher {
-    private String id;
+    @Id
+    private Integer id;
     private Integer user_id;
 }
