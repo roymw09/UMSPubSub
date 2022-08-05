b10a241fc99115ab93f4b3584b45edc6666987c0
roywilliams <roymw09@gmail.com>
2022-08-05 10:43:48 -0230

    Removed user roles

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -5,11 +5,3 @@ CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     username VARCHAR(100) NOT NULL UNIQUE
 );
-
-CREATE TABLE user_roles (
-    id SERIAL PRIMARY KEY,
-    user_id INT NOT NULL,
-    role_id INT UNIQUE,
-    role VARCHAR(50),
-    description VARCHAR(250)
-)
